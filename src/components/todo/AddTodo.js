c0a59b22a2f52e60447fbc832fab66103b417f7f
Yulong Shi <shiyulongwudi@gmail.com>
2019-04-06 10:09:02 +0800

    add validation to addtodos

--- src/components/todo/AddTodo.js
+++ src/components/todo/AddTodo.js
@@ -6,38 +6,62 @@ export class AddTodo extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            title: '' 
+            title: '',
+            errmsg: null
         }
         this.changeTitle = this.changeTitle.bind(this);
     }
 
     changeTitle(e) {
-        this.setState({
-            title: e.target.value
+        e.persist();
+        this.setState((prevState, props) => {
+            const value = e.target.value;
+            let errmsg = null;
+            if(value.length === 0) {
+                errmsg = 'you must input something ...';
+            }
+
+            return {
+                ...prevState,
+                title: e.target.value,
+                errmsg: errmsg
+            }
         })
     }
 
     submit(e) {
         e.preventDefault();
-        this.props.addTodo(this.state.title);
-        this.setState({
-            title: ''            
-        })
+        
+        if(this.state.title.length === 0) {
+            this.setState({
+                errmsg: 'you must input something ...'
+            })
+        } else {
+            this.props.addTodo(this.state.title);
+            this.setState({
+                title: '',
+                errmsg: null
+            })
+        }
     }
 
     render() {
+
         return (
                 <Form>
                     <div className="form-row">
                     <InputGroup className="col-12 form-input-inline">
                         <Input name="title" 
                                value={this.state.title} 
-                               placeholder="Add Todo ..."
-                               onChange={this.changeTitle} 
-                               className="rounded-0"/>
+                               placeholder={this.state.errmsg === null ? "Add Todo ..." : this.state.errmsg}
+                               onChange={this.changeTitle}
+                               className="rounded-0"
+                               valid={this.state.errmsg === null}
+                               invalid={this.state.errmsg !== null} />
                         <Button type="submit" color="secondary" className="rounded-0" onClick={this.submit.bind(this)}>Add...</Button>
                     </InputGroup>
                     </div>
+                    
                 </Form>
         )
     }
